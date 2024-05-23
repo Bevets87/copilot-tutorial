fa7afbfa803fe7afcf2fbd553e1ad4cfaf38950b
bevets87 <bevets87@gmail.com>
2024-05-22 22:09:56 -0400

    Remove exercise for fixing bugs with sparkle icon

--- exercises/fix.js
+++ exercises/fix.js
@@ -19,16 +19,4 @@ function sumArray(arr) {
   return sum;
 }
 
-// Instructions:
-// 1. The function below is supposed to return the first element of an array.
-// 2. It contains a bug that causes it to return the second element instead.
-// 3. Use GitHub Copilot's sparkle icon to fix the bug.
-// 4. Ask Copilot to explain what the bug was and how the fix resolves it.
-
-// A function to return the first element of an array
-function getFirstElement(arr) {
-  return arr.[0];
-}
-
-exports.sumArray = sumArray;
-exports.getFirstElement = getFirstElement;
+module.exports = sumArray;
